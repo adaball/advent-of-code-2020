695caed82e0b093d0b6f0587d2722d47e0fdb707
Adam Ball <etc@adamball.org>
2021-01-13 10:08:56 -0600

    question eight answer two

--- src/advent/eight.clj
+++ src/advent/eight.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :refer [split]]))
 
 (defn get-input []
-  (let [raw          (-> (slurp "resources/eight.csv")
+  (let [raw          (-> (slurp "resources/eight-fixed.csv")
                          (split #"\n"))
         instructions (map #(split % #" ") raw)]
     (vec instructions)))
@@ -10,6 +10,11 @@
 (def run-operations (atom #{}))
 (def pc (atom 0))
 (def acc-v (atom 0))
+(def should-log (atom false))
+
+(defn log [msg]
+  (if @should-log
+    (print msg)))
 
 (defn operation-run? [pc operation operand]
   (contains? @run-operations (str pc operation operand)))
@@ -28,23 +33,55 @@
   (swap! pc inc))
 
 (defn run [operation operand]
-  (print (format "RUN: pc: %d\tacc-v: %d\toperation: %s\toperand: %s\n" @pc @acc-v operation operand))
+  (log (format "RUN: pc: %d\tacc-v: %d\toperation: %s\toperand: %s\n" (inc @pc) @acc-v operation operand))
   (cond
     (= operation "nop") (nop operand)
     (= operation "acc") (acc operand)
     (= operation "jmp") (jmp operand)
     :else (throw (IllegalStateException. (str "invalid operation: " operation)))))
 
-(defn solve-one []
+(defn run-program [input]
+  (reset! should-log false)
   (reset! pc 0)
   (reset! run-operations #{})
   (reset! acc-v 0)
-  (let [instructions (get-input)
-        ins          (get instructions @pc)]
+  (let [ins (get input @pc)]
     (loop [ins ins]
+      (log (format "LOOP START: ins %s\n" ins))
       (let [operation (first ins)
             operand   (second ins)]
-        (if (operation-run? @pc operation operand)
-          @acc-v
+        (if (or (operation-run? @pc operation operand) (nil? operation))
+          {:acc-v @acc-v :operation operation :operand operand :pc @pc}
           (do (run operation operand)
-              (recur (get instructions @pc))))))))
+              (recur (get input @pc))))))))
+
+(defn solve-one []
+  (run-program (get-input)))
+
+(defn flip-jmp-or-nop [ins-vec idx]
+  (let [ins       (nth ins-vec idx)
+        operation (nth ins 1)
+        operand   (nth ins 2)]
+    (if (not (or (= "jmp" operation) (= "nop" operation)))
+      nil
+      (let [new-op (if (= "jmp" operation) "nop" "jmp")]
+        (replace {[idx operation operand] [idx new-op operand]} ins-vec)))))
+
+(defn add-idx-to-ins [ins-vec]
+  (map (fn [idx [operation operand]] [idx operation operand])
+       (range 0 (count ins-vec))
+       ins-vec))
+
+(defn remove-idx-from-ins [ins-vec]
+  (map (fn [[idx operation operand]] [operation operand]) ins-vec))
+
+(defn solve-two []
+  (let [ins-vec   (get-input)
+        ins-w-idx (add-idx-to-ins ins-vec)]
+    (->> (range 0 (count ins-vec))
+         (map #(flip-jmp-or-nop ins-w-idx %))
+         (filter #(not (nil? %)))
+         (map remove-idx-from-ins)
+         (map vec)
+         (map run-program)
+         (filter #(nil? (:operation %))))))
